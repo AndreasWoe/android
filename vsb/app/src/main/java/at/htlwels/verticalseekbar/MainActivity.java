af0b31791c709a8b5363044c69e0c99b83ac847d
Andreas Wögerbauer <andreas.woegerbauer@htl-wels.at>
2022-11-16 11:51:11 +0100

    initial

---
+++ vsb/app/src/main/java/at/htlwels/verticalseekbar/MainActivity.java
@@ -0,0 +1,40 @@
+package at.htlwels.verticalseekbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        VerticalSeekBar vsb = findViewById(R.id.seekBar);
+
+        vsb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress,
+                                          boolean fromUser) {
+                TextView txtProgress = findViewById(R.id.textView);
+                txtProgress.setText(progress+"");
+            }
+        });
+    }
+}
